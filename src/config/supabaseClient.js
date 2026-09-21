3ca752e6cce315c3819ee847404a7fa756e9ef01
DrySafe <149807603+DrySafe@users.noreply.github.com>
2026-09-21 18:05:06 +0000

    Ajustando api key supabase

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Substitua pelas chaves do seu projeto no painel do Supabase
 const supabaseUrl = 'https://gptofnrdirvycesycidw.supabase.co'
-const supabaseAnonKey = 'sb_publishable_BvhlrisJoysWz9NnzvrpMw__Px2W_RJ'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwdG9mbnJkaXJ2eWNlc3ljaWR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5ODY0MzAsImV4cCI6MjEwNTU2MjQzMH0.cQ3hoBiMUZUPh9twUxjFlCl5yu9rQ6WJs8VdsCv2_XQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
